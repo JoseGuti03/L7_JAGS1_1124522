1490baee840983c3eb2ad7aec5d0727b09a6ff8c
Giovani Gutierrez <Giovani Gutierrez@DELL>
2022-09-30 23:07:13 -0600

    Agregar archivos de proyecto.

---
+++ L7_JAGS1_1124522/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L7_JAGS1_1124522
+{
+    internal class Program
+    {
+        static void Main(string[] args);
+        int cant;
+        int a, b, c;
+        Console.WriteLine("Ingresar numero: ");
+            cant = Int32.Parse(Console.ReadLine());
+            a = 0; b = 1;
+            Console.Write(a + " " + b + " " );
+            for (int k = 3; k<= cant; k++)
+            {
+                c = a + b;
+                Console.Write(c + " ");
+                a = b;
+                b = c;
+            }
+    Console.ReadKey();
+        }
+    }
+}
